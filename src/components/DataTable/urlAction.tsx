13f22688970e92f1174ce8ef79db6009768dd279
le tu <lethucthanhtu@gmail.com>
2025-03-23 00:03:21 +0700

    feature: fix: UI action button

--- src/components/DataTable/urlAction.tsx
+++ src/components/DataTable/urlAction.tsx
@@ -51,70 +51,72 @@ export default function UrlAction({
 
 	return (
 		<>
-			<DropdownMenu>
-				<DropdownMenuTrigger asChild>
-					<Button variant='ghost' className={cn('size-8 p-0', className)}>
-						<span className='sr-only'>Open menu</span>
-						<MoreHorizontal />
-					</Button>
-				</DropdownMenuTrigger>
-				<DropdownMenuContent align='end'>
-					<DropdownMenuLabel>Actions</DropdownMenuLabel>
-					<DropdownMenuSeparator />
-					<Link to='/link/$id' params={{ id: url.shorten_url }}>
-						<DropdownMenuItem>
-							<FileChartLine />
-							View details
+			<div className={cn('', className)}>
+				<DropdownMenu>
+					<DropdownMenuTrigger asChild>
+						<Button variant='ghost' className={cn('size-8 p-0', '')}>
+							<span className='sr-only'>Open menu</span>
+							<MoreHorizontal />
+						</Button>
+					</DropdownMenuTrigger>
+					<DropdownMenuContent align='end'>
+						<DropdownMenuLabel>Actions</DropdownMenuLabel>
+						<DropdownMenuSeparator />
+						<Link to='/link/$id' params={{ id: url.shorten_url }}>
+							<DropdownMenuItem>
+								<FileChartLine />
+								View details
+							</DropdownMenuItem>
+						</Link>
+						<DropdownMenuItem onClick={() => setIsUpdateDialogOpen(true)}>
+							<Edit />
+							Edit URL
 						</DropdownMenuItem>
-					</Link>
-					<DropdownMenuItem onClick={() => setIsUpdateDialogOpen(true)}>
-						<Edit />
-						Edit URL
-					</DropdownMenuItem>
-					<DropdownMenuSeparator />
-					<DropdownMenuItem
-						onClick={() =>
-							navigator.clipboard.writeText(
-								makeURL(url.custom_url || url.shorten_url)
-							)
-						}
-					>
-						<Copy />
-						Copy shorten URL
-					</DropdownMenuItem>
-					<DropdownMenuItem onClick={() => setIsQrCodeDialogOpen(true)}>
-						<QrCode />
-						Show QR code
-					</DropdownMenuItem>
-					<DropdownMenuSeparator />
-					<DropdownMenuItem
-						onClick={() =>
-							fnUpdateActive(url.id, !url.active).then(() => fetchURLs())
-						}
-						className={cn('', url.active ? 'text-red-500' : 'text-green-500')}
-					>
-						<Power />
-						{url.active ? <>De-Active URL</> : <>Active URL</>}
-					</DropdownMenuItem>
-					<DropdownMenuItem
-						onClick={() => fnDelete(url.id).then(() => fetchURLs())}
-						className='text-red-500'
-					>
-						<Trash /> Remove
-					</DropdownMenuItem>
-				</DropdownMenuContent>
-			</DropdownMenu>
-			<QRDialog
-				url={url}
-				isOpen={isQrCodeDialogOpen}
-				onClose={() => setIsQrCodeDialogOpen(false)}
-			/>
-			<EditDialog
-				url={url}
-				isOpen={isUpdateDialogOpen}
-				onClose={() => setIsUpdateDialogOpen(false)}
-				fetchURLs={fetchURLs}
-			/>
+						<DropdownMenuSeparator />
+						<DropdownMenuItem
+							onClick={() =>
+								navigator.clipboard.writeText(
+									makeURL(url.custom_url || url.shorten_url)
+								)
+							}
+						>
+							<Copy />
+							Copy shorten URL
+						</DropdownMenuItem>
+						<DropdownMenuItem onClick={() => setIsQrCodeDialogOpen(true)}>
+							<QrCode />
+							Show QR code
+						</DropdownMenuItem>
+						<DropdownMenuSeparator />
+						<DropdownMenuItem
+							onClick={() =>
+								fnUpdateActive(url.id, !url.active).then(() => fetchURLs())
+							}
+							className={cn('', url.active ? 'text-red-500' : 'text-green-500')}
+						>
+							<Power />
+							{url.active ? <>De-Active URL</> : <>Active URL</>}
+						</DropdownMenuItem>
+						<DropdownMenuItem
+							onClick={() => fnDelete(url.id).then(() => fetchURLs())}
+							className='text-red-500'
+						>
+							<Trash /> Remove
+						</DropdownMenuItem>
+					</DropdownMenuContent>
+				</DropdownMenu>
+				<QRDialog
+					url={url}
+					isOpen={isQrCodeDialogOpen}
+					onClose={() => setIsQrCodeDialogOpen(false)}
+				/>
+				<EditDialog
+					url={url}
+					isOpen={isUpdateDialogOpen}
+					onClose={() => setIsUpdateDialogOpen(false)}
+					fetchURLs={fetchURLs}
+				/>
+			</div>
 		</>
 	);
 }
