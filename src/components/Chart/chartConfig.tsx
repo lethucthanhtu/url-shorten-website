1704ecb55b0e166922a565671b8706ab2e023584
le tu <lethucthanhtu@gmail.com>
2025-03-31 10:51:56 +0700

    feature: loading wrapper

--- src/components/Chart/chartConfig.tsx
+++ src/components/Chart/chartConfig.tsx
@@ -1,8 +1,8 @@
 import { ChartConfig } from '@/components/ui/chart';
 
 export const engagementChartConfig = {
-	click: {
-		label: 'Visit',
-		color: 'hsl(var(--chart-1))',
+	clicks: {
+		label: 'Clicks',
+		color: 'hsl(var(--chart-2))',
 	},
 } satisfies ChartConfig;
